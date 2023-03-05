335138765280c12f8dbe2b06eac37c5eddd7808b
stefinfection <sjeangeo@gmail.com>
2023-03-04 18:54:26 -0700

    enable serial read

--- scripts/logBytes.py
+++ scripts/logBytes.py
@@ -6,6 +6,9 @@
 from datetime import datetime
 import serial
 
+# Functioning command:
+# screen /dev/ttyUSB0 115200
+
 
 def read_data(path, baud_rate, port_name):
     # Open file
@@ -14,15 +17,13 @@ def read_data(path, baud_rate, port_name):
     f.write("Trial " + now.strftime("%d/%m/%Y_%H:%M:%S") + "\n")
 
     # Configure + open serial instance
-    ser = serial.Serial(port='/dev/ttyAMA0', baudrate=115200)
-    ser.open()
+    ser = serial.Serial(port=port_name, baudrate=baud_rate)
 
-    # Idiot check
     if ser.is_open:
         print("Successfully opened port to Disco \n")
-        byte = ser.readline()
+        byte = ser.read()
         print("Read a byte\n")
-        f.write(byte)
+        f.write('{}'.format(int.from_bytes(byte,"big")))
     else:
         print("Could not interface with Disco... shutting down \n")
         f.write("Could not successfully open serial communication with port " + port_name + "at rate " + baud_rate)
@@ -35,5 +36,5 @@ def read_data(path, baud_rate, port_name):
 if __name__ == '__main__':
     dir = "/home/whiskers/Documents/"
     br = 115200
-    pn = "/dev/ttyAMA0"
+    pn = "/dev/ttyUSB0"
     read_data(dir, br, pn)
